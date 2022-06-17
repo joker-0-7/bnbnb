8726b481875149dbc7b303f38c729148ee3cd6ff
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2022-06-17 12:55:10 +0200

    Add files via upload

---
+++ the site/js/lop.js
@@ -0,0 +1,9 @@
+let outSideArr = ["الدمام","الرياض", "المدينة", "جدة"];
+let inSideArr = [];
+let inSide = document.querySelector(".inside");
+let outSide = document.querySelector(".outside");
+
+for (let i = 0; i < inSideArr.length; i++) {
+    let card = document.createElement("h1");
+    
+}
---
+++ the site/js/main.js
@@ -0,0 +1,15 @@
+// let spans = document.querySelectorAll(".span");
+// let counted = false
+// window.onload = function () {
+//     spans.forEach((span)=>{
+//         let end = span.getAttribute("data-num")
+//         let counter = setInterval(() => {
+//             if (counted === false) {
+//                 span.textContent ++
+//                 if (span.textContent == end) {
+//                     clearInterval(counter)
+//                 }
+//             }
+//         }, 2000 / end);
+//     })
+// }
